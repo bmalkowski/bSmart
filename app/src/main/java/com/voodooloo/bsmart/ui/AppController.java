a2b35a7ed5118fdbc68ec7192e95afc479faa96b
BAM <bmalkowski@gmail.com>
2015-02-19 22:27:25 -0600

    Had to switch to JDK 8_40 due to a Tab hashcode bug

--- app/src/main/java/com/voodooloo/bsmart/ui/AppController.java
+++ app/src/main/java/com/voodooloo/bsmart/ui/AppController.java
@@ -5,23 +5,12 @@ import com.google.common.eventbus.Subscribe;
 import com.voodooloo.bsmart.App;
 import com.voodooloo.bsmart.investments.Portfolio;
 import com.voodooloo.bsmart.investments.PortfolioData;
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
-import org.controlsfx.control.ButtonBar;
-import org.controlsfx.control.action.Action;
-import org.controlsfx.dialog.Dialog;
-import org.controlsfx.dialog.DialogAction;
-import org.controlsfx.validation.ValidationSupport;
-import org.controlsfx.validation.Validator;
 
 import javax.inject.Inject;
 import java.util.HashMap;
@@ -34,6 +23,7 @@ public class AppController implements Controller {
 
     BorderPane rootBorderPane;
     TabPane tabPane;
+    Tab overviewTab;
 
     @Inject
     public AppController(App app, PortfolioData portfolioData, ControllerFactory controllerFactory) {
@@ -47,12 +37,22 @@ public class AppController implements Controller {
     public void load() {
         app.bus().register(this);
 
-        Tab overviewTab = new Tab();
+        OverviewController overviewController = controllerFactory.overviewController();
+        InvestmentsController investmentsController = controllerFactory.investmentsController();
+
+        overviewTab = new Tab();
         overviewTab.setText("Overview");
+        overviewTab.setUserData(overviewController);
+        loadedControllers.put(overviewTab, overviewController);
+
+        Tab investmentsTab = new Tab();
+        investmentsTab.setText("Investments");
+        investmentsTab.setUserData(investmentsController);
+        loadedControllers.put(investmentsTab, investmentsController);
 
         tabPane = new TabPane();
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
-        tabPane.getTabs().addAll(overviewTab);
+        tabPane.getTabs().addAll(overviewTab, investmentsTab);
 
         tabPane.getSelectionModel().clearSelection();
         tabPane.getSelectionModel().selectedItemProperty().addListener(this::onTabSelected);
@@ -79,73 +79,22 @@ public class AppController implements Controller {
         }
 
         Controller newController = loadedControllers.get(newValue);
-        if (newController == null) {
-            Portfolio portfolio = (Portfolio)newValue.getUserData();
-            newController = portfolio == null ?
-                    controllerFactory.investmentsController() :
-                    controllerFactory.portfolioController(portfolio);
-            loadedControllers.put(newValue, newController);
-        }
-
         newController.load();
         newValue.setContent(newController.node());
-    }
-
-    void onAdd(ActionEvent event) {
-        Action saveAction = new DialogAction("Save", ButtonBar.ButtonType.OK_DONE);
-        ValidationSupport validationSupport = new ValidationSupport();
-        Portfolio.Builder builder = new Portfolio.Builder();
-
-        Text nameLabel = new Text("Name");
-        TextField nameInput = new TextField();
-        nameInput.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                if (!validationSupport.isInvalid()) {
-                    builder.name(newValue);
-                }
-            }
-        });
-
-        validationSupport.registerValidator(nameInput, Validator.createEmptyValidator("Name is required"));
-        validationSupport.invalidProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                saveAction.setDisabled(newValue);
-            }
-        });
-
-        GridPane gridPane = new GridPane();
-        GridPane.setConstraints(nameLabel, 0, 0);
-        GridPane.setConstraints(nameInput, 1, 0);
-        gridPane.getChildren().addAll(nameLabel, nameInput);
-
-        final Dialog dialog = new Dialog(null, "New Portfolio", false);
-        dialog.getStyleClass().add(Dialog.STYLE_CLASS_NATIVE);
-        dialog.setResizable(false);
-        dialog.setIconifiable(false);
-        dialog.setContent(gridPane);
-        dialog.getActions().setAll(saveAction, Dialog.ACTION_CANCEL);
-
-        Action response = dialog.show();
-        if (response == saveAction) {
-            portfolioData.insert(builder.build());
-        }
     }
 
     @Subscribe
     public void onEvent(PortfolioData.Event event) {
         ObservableList<Tab> tabs = tabPane.getTabs();
-        if (tabs.size() > 1) {
-            tabs.remove(1, tabs.size());
+        if (tabs.size() > 2) {
+            tabs.remove(2, tabs.size());
         }
 
         ImmutableList<Portfolio> portfolios = portfolioData.findAll();
         for (Portfolio porfolio : portfolios) {
             Tab tab = new Tab();
             tab.setText(porfolio.name);
-            tab.setContent(new Text(porfolio.name + " content"));
-            tab.setUserData(porfolio);
+            tab.setUserData(controllerFactory.portfolioController(porfolio));
             tabs.add(tab);
         }
     }
